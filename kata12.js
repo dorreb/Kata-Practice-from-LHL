f285daf7b23640fffc24f3f8279e52b869ec92a8
dorreb <ashley_ure@hotmail.com>
2023-04-03 10:46:58 -0400

    add: bakery kata

---
+++ kata12.js
@@ -0,0 +1,63 @@
+// We need to complete a function called chooseRecipe(), which will receive three parameters: - An array of ingredients in stock at Bakery A - An array of ingredients in stock at Bakery B - An array of recipe objects. Each recipe has a name property(string) and an ingredient property(array)
+
+// We are limiting our search to two ingredient recipes. We want to find a recipe that utilizes one ingredient from Bakery A and one from Bakery B.
+
+// Our chooseRecipe() function should return the name of the correct recipe.
+
+const ingredientCheck = function(bakery, ingredients) {
+  for (let i = 0; i < ingredients.length; i++) {
+    if (bakery.includes(ingredients[i])) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const chooseRecipe = function(bakeryA, bakeryB, recipes) {
+  for (let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+    const hasIngredientA = ingredientCheck(bakeryA, recipe.ingredients);
+    const hasIngredientB = ingredientCheck(bakeryB, recipe.ingredients);
+    if (hasIngredientA && hasIngredientB) {
+      return recipe.name;
+    }
+  }
+};
+
+let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
+let bakeryB = ['milk', 'butter', 'cream cheese'];
+let recipes = [
+  {
+    name: 'Coconut Sponge Cake',
+    ingredients: ['coconut', 'cake base']
+  },
+  {
+    name: 'Persian Cheesecake',
+    ingredients: ['saffron', 'cream cheese']
+  },
+  {
+    name: 'Custard Surprise',
+    ingredients: ['custard', 'ground beef']
+  }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+bakeryA = ['potatoes', 'bay leaf', 'raisins'];
+bakeryB = ['red bean', 'dijon mustard', 'apples'];
+recipes = [
+  {
+    name: 'Potato Ganache',
+    ingredients: ['potatoes', 'chocolate']
+  },
+  {
+    name: 'Sweet Fish',
+    ingredients: ['anchovies', 'honey']
+  },
+  {
+    name: "Nima's Famous Dijon Raisins",
+    ingredients: ['dijon mustard', 'raisins']
+  }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
